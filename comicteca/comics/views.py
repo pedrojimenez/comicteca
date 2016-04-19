79ecaf7e88bfeb490cbe50b361d63aa52f2ce7c4
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-04-19 20:11:13 +0200

    Add a default index view for the comicteca project

--- comicteca/comics/views.py
+++ comicteca/comics/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""Comic App views."""
 
-# Create your views here.
+from django.shortcuts import render
+from comics.models import Artist
+
+
+def index(request):
+    """Index view."""
+
+    artists_list = Artist.objects.order_by('-name')[:5]
+    context_dict = {'artists': artists_list}
+
+    # Render the response and send it back!
+    return render(request, 'comics/index.html', context_dict)
