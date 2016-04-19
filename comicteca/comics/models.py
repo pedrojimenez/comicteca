e97470f7d920f5ab20f6acd22d63a898ec3a5d68
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-04-19 18:32:50 +0200

    Added "Artist" model in comics app

--- comicteca/comics/models.py
+++ comicteca/comics/models.py
@@ -1,3 +1,24 @@
+"""Models for the comicteca project."""
 from django.db import models
-
+from django_countries.fields import CountryField
 # Create your models here.
+
+
+class Artist(models.Model):
+    """Artists model."""
+
+    name = models.CharField(max_length=30)
+    nationality = CountryField(blank_label='(select country)')
+    birthdate = models.DateField(blank=True, null=True)
+    deathdate = models.DateField(blank=True, null=True)
+    biography = models.TextField(blank=True, null=True)
+
+    def __unicode__(self):
+        """str/unicode function of Artists class."""
+        return self.name
+
+    class Meta:
+        """Meta class for Artist model."""
+
+        db_table = 'artists'
+        verbose_name_plural = "artists"
