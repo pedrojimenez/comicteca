456deda6791e52df145f38eaf4ff5ee5c86e4b4a
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-04-19 18:33:57 +0200

    Added "Artist" model to admin

--- comicteca/comics/admin.py
+++ comicteca/comics/admin.py
@@ -1,3 +1,7 @@
+"""Admin models for the Comics app in the Comicteca project."""
 from django.contrib import admin
+from comics.models import Artist
 
 # Register your models here.
+
+admin.site.register(Artist)
