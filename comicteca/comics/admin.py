9d2da4058ecc2df9b163e49b55abb93bd921365c
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-05-09 19:58:42 +0200

    Add "colection" model to admin in Comics app

--- comicteca/comics/admin.py
+++ comicteca/comics/admin.py
@@ -1,6 +1,7 @@
 """Admin models for the Comics app in the Comicteca project."""
 from django.contrib import admin
 from comics.models import Artist
+from comics.models import Colection
 
 # Register your models here.
 
@@ -8,4 +9,16 @@ class ArtistAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
 
+class ColectionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    list_display = ('name','subname','volume','colection_list')
+    search_fields = ['name','subname']
+    list_filter = ['name']
+    fieldsets = [
+            ('Nombre Coleccion', {'fields': ['name','volume','subname','max_numbers','pub_date','slug']}) ,
+    ]
+
+
 admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Colection, ColectionAdmin)
+
