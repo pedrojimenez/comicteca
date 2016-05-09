cc11247e9cbcea454cc055bb68bc56e36e80f481
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-05-09 19:59:34 +0200

    Add "colection" url matching in Comics app

--- comicteca/comics/urls.py
+++ comicteca/comics/urls.py
@@ -7,5 +7,6 @@ from comics import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^artist/(?P<artist_name_slug>[\w\-]+)/$', views.artist, name='artist'),
+    url(r'^colection/(?P<colection_name_slug>[\w\-]+)/$', views.colection, name='colection'),
     #url(r'^about/$', views.about, name='about'),
 )
