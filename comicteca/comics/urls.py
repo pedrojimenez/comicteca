7a6c4f8818445894570261f4b520a321c40f227f
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-05-16 17:17:32 +0200

    Add "add_artist" to the url matching

--- comicteca/comics/urls.py
+++ comicteca/comics/urls.py
@@ -7,6 +7,7 @@ from comics import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^artist/(?P<artist_name_slug>[\w\-]+)/$', views.artist, name='artist'),
+    url(r'^add_artist/$', views.add_artist, name='add_artist'),
     url(r'^colection/(?P<colection_name_slug>[\w\-]+)/$', views.colection, name='colection'),
-    #url(r'^about/$', views.about, name='about'),
+    url(r'^about/$', views.about, name='about'),
 )
