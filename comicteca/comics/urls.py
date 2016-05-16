f9d99e75cbd756a5f6c7bce52e3639ae4f7c171c
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-05-16 20:51:34 +0200

    Change to plural in Artist/Colection urls and in the main index html

--- comicteca/comics/urls.py
+++ comicteca/comics/urls.py
@@ -6,9 +6,9 @@ from comics import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^artist/(?P<artist_name_slug>[\w\-]+)/$', views.artist, name='artist'),
+    url(r'^artists/(?P<artist_name_slug>[\w\-]+)/$', views.artist, name='artist'),
     url(r'^add_artist/$', views.add_artist, name='add_artist'),
-    url(r'^colection/(?P<colection_name_slug>[\w\-]+)/$', views.colection, name='colection'),
+    url(r'^colections/(?P<colection_name_slug>[\w\-]+)/$', views.colection, name='colection'),
     url(r'^add_colection/$', views.add_colection, name='add_colection'),
     url(r'^publishers/(?P<publisher_name_slug>[\w\-]+)/$', views.publisher, name='publisher'),
     url(r'^about/$', views.about, name='about'),
