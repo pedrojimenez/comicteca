05e5f36e995e500b4331e7b0466824e19fece148
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-05-16 17:16:14 +0200

    Add "add_artist" method to views, and fix exta space in some lines

--- comicteca/comics/views.py
+++ comicteca/comics/views.py
@@ -3,14 +3,14 @@
 from django.shortcuts import render
 from comics.models import Artist
 from comics.models import Colection
+from comics.forms import ArtistForm
 
 
 def index(request):
     """Index view."""
-
     artists_list = Artist.objects.order_by('-name')[:5]
     colection_list = Colection.objects.order_by('-name')[:5]
-    context_dict = {'artists': artists_list,'colections': colection_list}
+    context_dict = {'artists': artists_list, 'colections': colection_list}
 
     # Render the response and send it back!
     return render(request, 'comics/index.html', context_dict)
@@ -20,25 +20,36 @@ def artist(request, artist_name_slug):
     context_dict = {}
     try:
         artist = Artist.objects.get(slug=artist_name_slug)
-        #ategory = Category.objects.get(slug=category_name_slug)
+        # category = Category.objects.get(slug=category_name_slug)
         context_dict['artist_name'] = artist.name
 
         # Retrieve all of the associated pages.
         # Note that filter returns >= 1 model instance.
-        #pages = Page.objects.filter(category=category)
+        # pages = Page.objects.filter(category=category)
 
         # Adds our results list to the template context under name pages.
-        #context_dict['pages'] = pages
-        # We also add the category object from the database to the context dictionary.
+        # context_dict['pages'] = pages
+        # We also add the category object from the database to the
+        # context dictionary.
         # We'll use this in the template to verify that the category exists.
         context_dict['artist'] = artist
     except Artist.DoesNotExist:
         # We get here if we didn't find the specified artist.
-        # Don't do anything - the template displays the "no category" message for us.
+        # Don't do anything - the template displays the "no category"
+        # message for us.
         pass
 
     # Go render the response and return it to the client.
     return render(request, 'comics/artist.html', context_dict)
+
+
+def add_artist(request):
+    if request.method == 'POST':
+        form = ArtistForm(request.POST)
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = ArtistForm()
+    return render(request, 'comics/add_artist.html', {'form': form})
 
 
 def colection(request, colection_name_slug):
@@ -49,7 +60,8 @@ def colection(request, colection_name_slug):
         context_dict['colection'] = colection
     except Colection.DoesNotExist:
         # We get here if we didn't find the specified Colection
-        # Don't do anything - the template displays the "no colection" message for us.
+        # Don't do anything - the template displays the "no colection"
+        # message for us.
         pass
 
     # Go render the response and return it to the client.
