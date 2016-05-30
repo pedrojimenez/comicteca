f7f4f65ea034602710c9d3cc617ebaa4dfa2f57f
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-05-30 18:13:13 +0200

    Add relations between Colection and Distributor models (m2m) to admin

--- comicteca/comics/admin.py
+++ comicteca/comics/admin.py
@@ -3,8 +3,19 @@ from django.contrib import admin
 from comics.models import Artist
 from comics.models import Colection
 from comics.models import Publisher
+from comics.models import Distributor, Editor
 
 # Register your models here.
+
+class EditorInline(admin.TabularInline):
+    model = Editor
+    extra = 1
+
+
+class DistributorInline(admin.TabularInline):
+    model = Distributor
+    extra = 1
+
 
 class ArtistAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
@@ -18,13 +29,16 @@ class ColectionAdmin(admin.ModelAdmin):
     fieldsets = [
             ('Nombre Coleccion', {'fields': ['name','volume','subname','max_numbers','pub_date','slug']}) ,
     ]
+    inlines = [DistributorInline, EditorInline]
+
+
 
 class PublisherAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
     list_display = ('name','history','start_date','end_date')
     search_fields = ['name']
 
+
 admin.site.register(Artist, ArtistAdmin)
 admin.site.register(Colection, ColectionAdmin)
 admin.site.register(Publisher, PublisherAdmin)
-
