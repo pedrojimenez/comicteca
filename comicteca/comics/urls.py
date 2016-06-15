f178129989a0d06f66911261f1691f40269260fe
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-06-15 20:24:43 +0200

    Set proper names for detailed views of model Artist/Colection/Publisher

--- comicteca/comics/urls.py
+++ comicteca/comics/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns(
     # artists
     url(r'artists/add/$', ArtistCreate.as_view(), name='artist_add'),
     url(r'^artists/(?P<artist_name_slug>[\w\-]+)/$', views.artist,
-        name='artist-detail'),
+        name='artist_detail'),
     url(r'^artists/(?P<slug>[\w\-]+)/edit/$',
         ArtistUpdate.as_view(), name='artist_update'),
     url(r'^add_artist/$', views.add_artist, name='add_artist'),
@@ -21,12 +21,12 @@ urlpatterns = patterns(
     # colections
     url(r'colections/add/$', ColectionCreate.as_view(), name='colection_add'),
     url(r'^colections/(?P<colection_name_slug>[\w\-]+)/$', views.colection,
-        name='colection'),
+        name='colection_detail'),
     url(r'^add_colection/$', views.add_colection, name='add_colection'),
 
     # publishers
     url(r'^publishers/(?P<publisher_name_slug>[\w\-]+)/$', views.publisher,
-        name='publisher'),
+        name='publisher_detail'),
     url(r'^add_publisher/$', views.add_publisher, name='add_publisher'),
 
     # utils
