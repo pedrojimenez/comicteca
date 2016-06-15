fadcd887dbab59598742512e42cc8aa23f191f1f
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-06-15 19:00:07 +0200

    Refactor Artist urls to call "class based views"

--- comicteca/comics/urls.py
+++ comicteca/comics/urls.py
@@ -1,16 +1,34 @@
+"""Urls for Comics app."""
+
 from django.conf.urls import patterns, url
-from django.conf.urls import include
+# from django.conf.urls import include
 
 from comics import views
+from comics.views import ArtistCreate, ArtistUpdate  # , ArtistDelete
+from comics.views import ColectionCreate
 
-
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', views.index, name='index'),
-    url(r'^artists/(?P<artist_name_slug>[\w\-]+)/$', views.artist, name='artist'),
+    # artists
+    url(r'artists/add/$', ArtistCreate.as_view(), name='artist_add'),
+    url(r'^artists/(?P<artist_name_slug>[\w\-]+)/$', views.artist,
+        name='artist-detail'),
+    url(r'^artists/(?P<slug>[\w\-]+)/edit/$',
+        ArtistUpdate.as_view(), name='artist_update'),
     url(r'^add_artist/$', views.add_artist, name='add_artist'),
-    url(r'^colections/(?P<colection_name_slug>[\w\-]+)/$', views.colection, name='colection'),
+
+    # colections
+    url(r'colections/add/$', ColectionCreate.as_view(), name='colection_add'),
+    url(r'^colections/(?P<colection_name_slug>[\w\-]+)/$', views.colection,
+        name='colection'),
     url(r'^add_colection/$', views.add_colection, name='add_colection'),
-    url(r'^publishers/(?P<publisher_name_slug>[\w\-]+)/$', views.publisher, name='publisher'),
+
+    # publishers
+    url(r'^publishers/(?P<publisher_name_slug>[\w\-]+)/$', views.publisher,
+        name='publisher'),
     url(r'^add_publisher/$', views.add_publisher, name='add_publisher'),
+
+    # utils
     url(r'^about/$', views.about, name='about'),
 )
