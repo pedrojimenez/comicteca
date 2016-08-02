e247410245b062f7f291672b5a3118b80181ad7f
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-08-02 20:53:12 +0200

    Some changes in admin to show proper distributor relation

--- comicteca/comics/admin.py
+++ comicteca/comics/admin.py
@@ -3,39 +3,48 @@ from django.contrib import admin
 from comics.models import Artist
 from comics.models import Colection
 from comics.models import Publisher
-from comics.models import Distributor, Editor
+# from comics.models import Distributor, Editor
 
 # Register your models here.
 
-class EditorInline(admin.TabularInline):
-    model = Editor
-    extra = 1
+
+# class EditorInline(admin.TabularInline):
+#     model = Editor
+#     extra = 1
+#     fields = ('id', 'colection', 'editorial','extrainfo')
 
 
-class DistributorInline(admin.TabularInline):
-    model = Distributor
-    extra = 1
+# class DistributorInline(admin.TabularInline):
+#     model = Distributor
+#     extra = 1
 
 
 class ArtistAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
 
 class ColectionAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-    list_display = ('name','subname','volume','colection_list')
-    search_fields = ['name','subname']
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'subname', 'volume', 'colection_list', 'get_distributor')
+    search_fields = ['name', 'subname']
     list_filter = ['name']
     fieldsets = [
-            ('Nombre Coleccion', {'fields': ['name','volume','subname','max_numbers','pub_date','slug']}) ,
+        ('Nombre Coleccion', {'fields': ['name', 'volume', 'subname',
+         'max_numbers', 'pub_date', 'slug', 'distributors', 'editors']}),
     ]
-    inlines = [DistributorInline, EditorInline]
+    # inlines = [DistributorInline, EditorInline]
+    # inlines = [EditorInline]
 
+    def get_distributor(self, obj):
+        return obj.distributors.name
+
+    get_distributor.admin_order_field = 'distributors'
+    get_distributor.short_description = 'Distributor Name'
 
 
 class PublisherAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-    list_display = ('name','history','start_date','end_date')
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'history', 'start_date', 'end_date')
     search_fields = ['name']
 
 
