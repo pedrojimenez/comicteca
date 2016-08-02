d53618118505cd3dda8a1c2263ca5aba18950a01
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-08-02 20:45:24 +0200

    Rewriting of "publisher" model, commenting "Distributor and Editor"

--- comicteca/comics/models.py
+++ comicteca/comics/models.py
@@ -18,7 +18,7 @@ class Artist(models.Model):
     def get_absolute_url(self):
         """."""
         # return reverse('artist-detail', kwargs={'pk': self.pk})
-        return reverse('artist-detail', kwargs={'artist_name_slug': self.slug})
+        return reverse('artist_detail', kwargs={'artist_name_slug': self.slug})
 
     def __unicode__(self):
         """str/unicode function of Artists class."""
@@ -32,61 +32,9 @@ class Artist(models.Model):
     class Meta:
         """Meta class for Artist model."""
 
-        db_table = 'artists'
+        # db_table = 'artists'
         verbose_name_plural = "artists"
         unique_together = ("name", "nationality", "birthdate")
-
-
-class Colection(models.Model):
-    """Colection model."""
-
-    name = models.CharField(max_length=50)
-    subname = models.CharField(max_length=50, blank=True)
-    volume = models.IntegerField(default=1)
-    max_numbers = models.IntegerField(default=0)
-    numbers = models.IntegerField(default=0)
-    language = CountryField(blank_label='(select country)', default='ES')
-    pub_date = models.DateField(blank=True, null=True)
-    slug = models.SlugField()
-
-    # pub_list = models.TextField(blank=True, null=True, default='')
-
-    # Relations
-    distributors = models.ManyToManyField('Publisher',
-                                          related_name='Distributors',
-                                          through='Distributor')
-    editors = models.ManyToManyField('Publisher',
-                                     related_name='Publishers',
-                                     through='Editor')
-
-    def __unicode__(self):
-        """str/unicode function of Colection class."""
-        str_temp = self.name
-        if self.subname:
-            str_temp = str_temp + " - " + self.subname
-        str_temp = str_temp + " - Vol " + str(self.volume)
-        return str_temp
-
-    def save(self, *args, **kwargs):
-        """Overwriting of save function in Colection class."""
-        self.slug = slugify(self.name + ' v' + str(self.volume))
-        # self.pub_list = Distributor.objects.filter(colection=self)
-
-        super(Colection, self).save(*args, **kwargs)
-
-    def colection_list(self):
-        """Admin site method."""
-        if (self.max_numbers != 0) and (self.max_numbers == self.numbers):
-            return "Complete"
-        else:
-            return str(self.numbers) + "/" + str(self.max_numbers)
-
-    class Meta:
-        """Meta class for Colection model."""
-
-        db_table = 'colections'
-        verbose_name_plural = "colections"
-        unique_together = ("name", "volume")
 
 
 class Publisher(models.Model):
@@ -103,8 +51,8 @@ class Publisher(models.Model):
     def get_absolute_url(self):
         """."""
         # return reverse('artist-detail', kwargs={'pk': self.pk})
-        return reverse('publisher_detail', kwargs={'publisher_name_slug': self.slug})
-
+        return reverse('publisher_detail',
+                       kwargs={'publisher_name_slug': self.slug})
 
     def __unicode__(self):
         """str/unicode function of Publisher class."""
@@ -116,29 +64,88 @@ class Publisher(models.Model):
         super(Publisher, self).save(*args, **kwargs)
 
 
-class Distributor(models.Model):
-    """Distributor intermediate model."""
+class Colection(models.Model):
+    """Colection model."""
 
-    colection = models.ForeignKey(Colection, default=4)
-    editorial = models.ForeignKey(Publisher)
-    extrainfo = models.CharField(max_length=128, blank=True, null=True)
-    principal = models.BooleanField('Principal', default=True)
+    name = models.CharField(max_length=50)
+    subname = models.CharField(max_length=50, blank=True)
+    volume = models.IntegerField(default=1)
+    max_numbers = models.IntegerField(default=0)
+    numbers = models.IntegerField(default=0)
+    language = CountryField(blank_label='(select country)', default='ES')
+    pub_date = models.DateField(blank=True, null=True)
+    slug = models.SlugField()
+
+    # Relations
+    distributors = models.ForeignKey(Publisher, on_delete=models.CASCADE,
+                                     default='Marvel')
+    # distributors = models.ManyToManyField('Publisher',
+    #                                       related_name='Distributors',
+    #                                       through='Distributor')
+    editors = models.ManyToManyField(Publisher,
+                                     related_name='Publishers')
+    #                                through='Editor')
+
+    def __unicode__(self):
+        """str/unicode function of Colection class."""
+        str_temp = self.name
+        if self.subname:
+            str_temp = str_temp + " - " + self.subname
+        str_temp = str_temp + " - Vol " + str(self.volume)
+        return str_temp
+
+    def save(self, *args, **kwargs):
+        """Overwriting of save function in Colection class."""
+        self.slug = slugify(self.name + ' v' + str(self.volume))
+        # self.pub_list = Distributor.objects.filter(colection=self)
+
+        super(Colection, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        """."""
+        # return reverse('artist-detail', kwargs={'pk': self.pk})
+        return reverse('colection_detail',
+                       kwargs={'colection_name_slug': self.slug})
+
+    def colection_list(self):
+        """Admin site method."""
+        if (self.max_numbers != 0) and (self.max_numbers == self.numbers):
+            return "Complete"
+        else:
+            return str(self.numbers) + "/" + str(self.max_numbers)
 
     class Meta:
-        """Meta class for intermediate Distributor model."""
+        """Meta class for Colection model."""
 
-        auto_created = True
+        # db_table = 'colections'
+        verbose_name_plural = "colections"
+        unique_together = ("name", "volume")
 
 
-class Editor(models.Model):
-    """Editor intermediate model."""
-
-    colection = models.ForeignKey(Colection, default=4)
-    editorial = models.ForeignKey(Publisher)
-    extrainfo = models.CharField(max_length=128, blank=True, null=True)
-    principal = models.BooleanField('Principal', default=True)
-
-    class Meta:
-        """Meta class for intermediate Editor model."""
-
-        auto_created = True
+# class Distributor(models.Model):
+#     """Distributor intermediate model."""
+#
+#     colection = models.ForeignKey(Colection, default=4)
+#     editorial = models.ForeignKey(Publisher)
+#     extrainfo = models.CharField(max_length=128, blank=True, null=True)
+#     principal = models.BooleanField('Principal', default=True)
+#
+#     class Meta:
+#         """Meta class for intermediate Distributor model."""
+#
+#         auto_created = True
+#
+#
+# class Editor(models.Model):
+#     """Editor intermediate model."""
+#
+#     colection = models.ForeignKey(Colection, default=4)
+#     editorial = models.ForeignKey(Publisher)
+#     extrainfo = models.CharField(max_length=128, blank=True, null=True)
+#     principal = models.BooleanField('Principal', default=True)
+#
+#     class Meta:
+#         """Meta class for intermediate Editor model."""
+#
+#         auto_created = True
+#         #db_table = 'colections_editors'
