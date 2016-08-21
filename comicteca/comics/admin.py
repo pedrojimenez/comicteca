2c360ec88eaf60a0e5990e4a83ff28a766c17a03
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-08-21 14:35:39 +0200

    Add "pub_date" field from Comic model to admin

--- comicteca/comics/admin.py
+++ comicteca/comics/admin.py
@@ -87,7 +87,7 @@ class ComicAdmin(admin.ModelAdmin):
     # prepopulated_fields = {'slug': ('name',)}
     list_display = ('get_colection_name', 'get_colection_volume',
                     'get_colection_distributor', 'number',
-                    'title', 'pages', 'inserted', 'updated')
+                    'title', 'pages', 'pub_date', 'inserted', 'updated')
     search_fields = ['slug', 'title']
     inlines = [ColaboratorInline]
 
