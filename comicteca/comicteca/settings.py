962e68910d23065c68a5114469be4de141d553fe
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-09-08 20:05:21 +0200

    Add "media folders" to Settings and the context_processors

--- comicteca/comicteca/settings.py
+++ comicteca/comicteca/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',  # set this explicitly
             ],
         },
     },
@@ -114,5 +115,10 @@ STATIC_URL = '/static/'
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-            STATIC_PATH,
-            ]
+    STATIC_PATH, ]
+
+# Media files
+MEDIA_URL = '/media/'
+
+# Absolute path to the media directory
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
