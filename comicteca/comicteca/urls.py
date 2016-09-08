e21347b035f162a60d8f4b01fd34d6ad2e7e8b04
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-09-08 20:06:10 +0200

    Set thr "/media/" url to be served from the root project

--- comicteca/comicteca/urls.py
+++ comicteca/comicteca/urls.py
@@ -1,4 +1,4 @@
-"""comicteca URL Configuration
+"""comicteca URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -13,10 +13,16 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^comicteca/', include('comics.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        '', (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+             {'document_root': settings.MEDIA_ROOT}))
