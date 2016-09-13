0e2a7c9c54fc9e0452200c6a6bf144854aec5a93
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-09-13 20:09:29 +0200

    Add comics/publishers/artists stats to templatetag and show them in index

--- comicteca/comics/templatetags/comicteca_stats.py
+++ comicteca/comics/templatetags/comicteca_stats.py
@@ -1,5 +1,5 @@
 from django import template
-from ..models import Colection
+from ..models import Colection, Comic, Publisher, Artist
 
 register = template.Library()
 
@@ -7,3 +7,18 @@ register = template.Library()
 @register.simple_tag
 def total_colections():
     return Colection.objects.all().count()
+
+
+@register.simple_tag
+def total_comics():
+    return Comic.objects.all().count()
+
+
+@register.simple_tag
+def total_publishers():
+    return Publisher.objects.all().count()
+
+
+@register.simple_tag
+def total_artists():
+    return Artist.objects.all().count()
