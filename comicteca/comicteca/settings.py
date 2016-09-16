033df5f0d02640debc7559a3255d5e3b0d19ca2b
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-09-16 18:25:27 +0200

    Add the "sorl.thumbnail" app to settings and requirements

--- comicteca/comicteca/settings.py
+++ comicteca/comicteca/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_countries',
     'comics',
+    'sorl.thumbnail',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -73,7 +74,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',  # set this explicitly
+                # set this explicitly
+                'django.template.context_processors.media',
             ],
         },
     },
