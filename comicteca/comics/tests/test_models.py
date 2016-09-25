5caa232fc17104cc5d55d80be903c4ca67776864
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-09-25 11:56:00 +0200

    Refactor of "test_models"

--- comicteca/comics/tests/test_models.py
+++ comicteca/comics/tests/test_models.py
@@ -6,14 +6,48 @@ from comics.models import Colection
 from comics.models import Publisher
 
 
+# ------------------------------------------------------------------ #
+#
+#                         Auxiliar Functions
+#
+# ------------------------------------------------------------------ #
+def create_artist(name='Test Artist', nationality='ES'):
+        """Create an Artist external function."""
+        bio = 'This is a test biography for Mr <{}>'.format(name)
+        return Artist.objects.create(name=name,
+                                     nationality=nationality,
+                                     biography=bio)
+
+
+def create_publisher(name='Test Publisher', nationality='ES'):
+        """Create a Publisher external function."""
+        history = 'This is a test history for <{}>'.format(name)
+        return Publisher.objects.create(name=name,
+                                        nationality=nationality,
+                                        history=history)
+
+
+def create_colection(name='Test Colection', subname='extra subname',
+                     vol=1, language='ES', distributor_id='1',
+                     colection_type='Regular', max_numbers=12):
+        """Create a Colection external function."""
+        return Colection.objects.create(name=name,
+                                        subname=subname,
+                                        volume=vol,
+                                        language=language,
+                                        max_numbers=max_numbers,
+                                        colection_type=colection_type,
+                                        distributor=distributor_id,
+                                        )
+
+
+# ------------------------------------------------------------------ #
+#
+#                            Artist Tests
+#
+# ------------------------------------------------------------------ #
 class ArtistTest(TestCase):
     """Artist class tests."""
-
-    def create_artist(self, name='Test Artist', nationality='ES'):
-        """Internal method to create an artist model."""
-        bio = 'This is a test biography for Mr <{}>'.format(name)
-        return Artist.objects.create(name=name, nationality=nationality,
-                                     biography=bio)
 
     def test_artist_creation(self):
         """Unitest for Artists creation.
@@ -22,16 +56,8 @@ class ArtistTest(TestCase):
         Artist 1: <Frank Miller> from <United States of America>
         Artist 2: <Salvador Larroca> from <Spain>
         """
-        a1 = self.create_artist('Frank Miller', 'US')
-        a2 = self.create_artist('Salvador Larroca', 'ES')
-
-        print
-        print "Artists creation tests . . ."
-        print
-        print "Artist 1: <{}> from <{}>".format(a1.name, a1.nationality.name)
-        print "     Bio: {}".format(a1.biography)
-        print "Artist 2: <{}> from <{}>".format(a2.name, a2.nationality.name)
-        print "     Bio: {}".format(a2.biography)
+        a1 = create_artist('Frank Miller', 'US')
+        a2 = create_artist('Salvador Larroca', 'ES')
 
         self.assertTrue(isinstance(a1, Artist))
         self.assertTrue(isinstance(a2, Artist))
@@ -39,44 +65,40 @@ class ArtistTest(TestCase):
         self.assertEqual(a2.nationality.name, 'Spain')
 
 
+# ------------------------------------------------------------------ #
+#
+#                            Colection Tests
+#
+# ------------------------------------------------------------------ #
 class ColectionTest(TestCase):
     """Colection class tests."""
 
-    def create_colection(self, name='Test Colection', subname='extra subname',
-                         vol=1, maxnumbers=12, numbers=12):
-        """Internal method to create a colection model."""
-        return Colection.objects.create(name=name, subname=subname, volume=vol,
-                                        max_numbers=maxnumbers,
-                                        numbers=numbers)
-
     def test_colection_creation(self):
         """Test for creating an object from model Colection."""
-        c1 = self.create_colection('Xmen', 'children of the atom', 2, 50, 30)
-        c2 = self.create_colection('Alpha Flight', 'canada heroes', 1, 60, 50)
+        p1 = create_publisher(name='TestPublisher002', nationality='ES')
+        c1 = create_colection(
+            name='Xmen', subname='children of the atom', vol=2,
+            language='ES', distributor_id=p1, colection_type='Regular',
+            max_numbers=50)
 
-        print
-        print "Colection creation tests . . ."
-        print
         self.assertTrue(isinstance(c1, Colection))
-        self.assertTrue(isinstance(c2, Colection))
-        # self.assertEqual(a1.nationality.name, 'United States of America')
-        # self.assertEqual(a2.nationality.name, 'Spain')
+        self.assertEqual(c1.language.name, 'Spain')
 
 
+# ------------------------------------------------------------------ #
+#
+#                            Publisher Tests
+#
+# ------------------------------------------------------------------ #
 class PublisherTest(TestCase):
     """Publisher class tests."""
 
-    def create_publisher(self, name='Test Publisher', history='My test story'):
-        """Internal method to create a colection model."""
-        return Publisher.objects.create(name=name, history=history)
-
     def test_publisher_creation(self):
         """Test for creating an object from model Publisher."""
-        p1 = self.create_publisher('Marvel', 'the big factory')
-        p2 = self.create_publisher('DC', 'second on flight')
+        p1 = create_publisher(name='Marvel', nationality='US')
+        p2 = create_publisher(name='Forum', nationality='ES')
 
-        print
-        print "Publisher creation tests . . ."
-        print
         self.assertTrue(isinstance(p1, Publisher))
         self.assertTrue(isinstance(p2, Publisher))
+        self.assertEqual(p1.nationality.name, 'United States of America')
+        self.assertEqual(p2.nationality.name, 'Spain')
