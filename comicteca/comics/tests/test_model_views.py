5620f3ebcc424568a825bf5e65edef304a30dfd7
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-09-26 00:07:44 +0200

    Refactor "model_views" test

--- comicteca/comics/tests/test_model_views.py
+++ comicteca/comics/tests/test_model_views.py
@@ -1,85 +1,83 @@
 """Test views of Comics app."""
 
 from django.test import TestCase
-
-from django.template.defaultfilters import slugify
 from django.conf import settings as mysettings
-from comics.models import Publisher
-from comics.models import Colection
-from comics.models import Artist
+
+from comics.tests.common import create_artist
+from comics.tests.common import create_publisher
+from comics.tests.common import create_colection
+# from comics.tests.common import create_comic
 
 proj_name = mysettings.PROJECT_NAME
 
 
-class PublisherViewsTest(TestCase):
-    """Publisher Views class tests."""
-
-    def test_publisher_view(self):
-            """Test for checking publisher views."""
-            publisher_name = "Marvel Comics"
-            publisher_name_slug = slugify(publisher_name)
-            publisher_story = "my publisher story"
-            p1 = Publisher.objects.create(name=publisher_name,
-                                          history=publisher_story)
-
-            p1_url = '/{}/publishers/{}/'.format(proj_name,
-                                                 publisher_name_slug)
-            response = self.client.get(p1_url)
-
-            print
-            print "Publisher view tests . . ."
-            print "    getting <{}> view page . . .".format(p1)
-            print "    checking {}".format(p1_url)
-            print "    response: {}".format(response.status_code)
-
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response,
-                                    template_name='comics/publisher.html')
-
-
-class ColectionViewsTest(TestCase):
-    """Publisher Views class tests."""
-
-    def test_colection_view(self):
-            """Test for checking publisher views."""
-            colection_name = "The amazing Spiderman"
-            colection_name_slug = slugify(colection_name)
-            c1 = Colection.objects.create(name=colection_name)
-            c1_url = '/{}/colections/{}/'.format(proj_name,
-                                                 colection_name_slug)
-            response = self.client.get(c1_url)
-
-            print
-            print "Colection view tests . . ."
-            print "    getting <{}> view page . . .".format(c1)
-            print "    checking {}".format(c1_url)
-            print "    response: {}".format(response.status_code)
-
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response,
-                                    template_name='comics/colection.html')
-
-
+# ------------------------------------------------------------------ #
+#
+#                            Artist Tests
+#
+# ------------------------------------------------------------------ #
 class ArtistViewsTest(TestCase):
     """Artist Views class tests."""
 
     def test_artist_view(self):
-            """Test for checking publisher views."""
-            artist_name = "Stan Lee"
-            artist_nationality = "US"
-            artist_name_slug = slugify(artist_name)
-            a1 = Artist.objects.create(name=artist_name,
-                                       nationality=artist_nationality)
-            a1_url = '/{}/artists/{}/'.format(proj_name,
-                                              artist_name_slug)
-            response = self.client.get(a1_url)
+        """Test for checking publisher views."""
+        a1 = create_artist('Frank Miller', 'US')
+        a1_url = '/{}/artists/{}/'.format(proj_name, a1.slug)
+        response = self.client.get(a1_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+                                template_name='comics/artist.html')
 
-            print
-            print "Artist view tests . . ."
-            print "    getting <{}> view page . . .".format(a1)
-            print "    checking {}".format(a1_url)
-            print "    response: {}".format(response.status_code)
 
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response,
-                                    template_name='comics/artist.html')
+# ------------------------------------------------------------------ #
+#
+#                            Publisher Tests
+#
+# ------------------------------------------------------------------ #
+class PublisherViewsTest(TestCase):
+    """Publisher Views class tests."""
+
+    def test_publisher_view(self):
+        """Test for checking publisher views."""
+        p1 = create_publisher(name='DC Comics', nationality='US')
+        p1_url = '/{}/publishers/{}/'.format(proj_name, p1.slug)
+        response = self.client.get(p1_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+                                template_name='comics/publisher.html')
+
+
+# ------------------------------------------------------------------ #
+#
+#                            Colection Tests
+#
+# ------------------------------------------------------------------ #
+class ColectionViewsTest(TestCase):
+    """Colection Views class tests."""
+
+    def test_colection_view(self):
+        """Test for checking publisher views."""
+        p1 = create_publisher(name='TestPublisher009', nationality='ES')
+        c1 = create_colection(
+            name='The Amazing Spiderman', subname='Peter Parker is...', vol=1,
+            language='ES', distributor_id=p1, colection_type='Regular',
+            max_numbers=50)
+        c1_url = '/{}/colections/{}/'.format(proj_name, c1.slug)
+        response = self.client.get(c1_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+                                template_name='comics/colection.html')
+
+
+# ------------------------------------------------------------------ #
+#
+#                            Comic Tests
+#
+# ------------------------------------------------------------------ #
+class ComicViewsTest(TestCase):
+    """Comic Views class tests."""
+
+    def test_comic_view(self):
+        """."""
+        pass
