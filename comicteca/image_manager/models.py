7b79c206bf4b31dfe57965f025ebdf8b9938227b
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-10-13 20:53:44 +0200

    Add class "ImageManager" and the first method: is_valid_image_extension

--- comicteca/image_manager/models.py
+++ comicteca/image_manager/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class ImageManager(models.Model):
+    """Class for managing all image operations."""
+
+    __valid_extensions = ['jpg', 'jpeg', 'png']
+
+    def is_valid_image_extension(self, url):
+        """Check if input url is a valid image."""
+        if not url:
+            msg = 'The given URL is empty'
+            print msg
+            return 1
+
+        # Get the last part of the url and check against
+        extension = url.rsplit('.', 1)[1].lower()
+
+        if extension not in self.__valid_extensions:
+            msg = 'The given URL does not match valid image extensions.'
+            print msg
+            return 1
+
+        return 0
