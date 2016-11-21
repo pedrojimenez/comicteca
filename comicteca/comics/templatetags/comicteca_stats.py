4ed46dd348479c5357f72b06a92bab6f988d5637
Pedro Jiménez Solís <lordpiotr@gmail.com>
2016-11-21 23:05:47 +0100

    Add templatetags for retail and paid currencies

--- comicteca/comics/templatetags/comicteca_stats.py
+++ comicteca/comics/templatetags/comicteca_stats.py
@@ -22,3 +22,23 @@ def total_publishers():
 @register.simple_tag
 def total_artists():
     return Artist.objects.count()
+
+
+@register.simple_tag
+def total_retail_currency():
+    collection_list = Colection.objects.all()
+    total_cash = 0
+    for collection in collection_list:
+        total_cash += collection.get_currency(unit='euros',
+                                              output_format='integer')
+    return total_cash
+
+
+@register.simple_tag
+def total_paid_currency():
+    collection_list = Colection.objects.all()
+    total_cash = 0
+    for collection in collection_list:
+        total_cash += collection.get_paid(unit='euros',
+                                          output_format='integer')
+    return total_cash
